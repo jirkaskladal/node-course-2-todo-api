5c7e507705663d7ad597e870a981dd698f85a86f
Skládal Jiří <JSkladal@merlin.cz>
2018-08-15 07:46:25 +0200

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -73,6 +74,34 @@ app.delete('/todos/:id', (req, res) => {
 		return res.status(200).send({todo});
 	}).catch((e) => {
 			res.status(400).send(e);
+	});
+});
+
+app.patch('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	var body = _.pick(req.body, ['text', 'completed']);
+
+  // validate the id -> not valid? return 404
+	if (!ObjectID.isValid(id)) {
+		console.log('ID not valid');
+		return res.status(404).send();
+	}
+
+	if (_.isBoolean(body.completed) && body.completed) {
+		body.completedAt = new Date().getTime();
+	} else {
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findOneAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+		if (!todo) {
+			return res.status(400).send();
+		}
+
+		res.send({todo});
+	}).catch((e) => {
+		res.status(400).send();
 	});
 });
 
